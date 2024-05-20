ea6f0a0691702084fc6fbfe08c62b8944e855ad9
lukewire <lukewire@dmtek.co.kr>
2024-05-20 16:55:50 +0900

    KakaoLogin Update .

---
+++ CSharpForMarkupLogin/Starter.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpForMarkupLogin;
+
+public class Starter
+{
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        _ = new App ()
+            .Run ();
+    }
+}
---
+++ CSharpForMarkupLogin/UI/Pages/LoginPage.cs
@@ -0,0 +1,16 @@
+﻿using CSharpMarkup.Wpf;
+using CSharpMarkup.WPF.Support;
+using static CSharpMarkup.Wpf.Helpers;
+namespace CSharpForMarkupLogin.Views;
+
+public class LoginPage : MarkupPage
+{
+    public LoginPage()
+    {
+        this.Build ();
+    }
+
+    public override void Build() => Content =
+        VStack ()
+        .Background("#fee500");
+}
