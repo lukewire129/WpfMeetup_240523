12281177424db59fcdc011c55a974af7cdb72400
lukewire129 <lukewire129@gmail.com>
2024-05-21 06:51:32 +0900

    Update .

---
+++ CSharpForMarkupLogin/UI/Pages/LoginPage.logic.cs
@@ -0,0 +1,11 @@
+﻿using CSharpForMarkupLogin.UI.Units;
+using CSharpMarkup.WPF.Support;
+namespace CSharpForMarkupLogin.Views;
+public partial class LoginPage : MarkupPage
+{
+    static PlaceholderTextBox tb;
+    public LoginPage()
+    {
+        this.Build ();
+    }
+}
