25ec4e394ff0f90312fc7222dc11a54a9067196a
lukewire <lukewire@dmtek.co.kr>
2024-05-21 08:42:42 +0900

    Update .

---
+++ CSharpForMarkupLogin/UI/Pages/MainPage.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+namespace CSharpForMarkupLogin.UI.Pages;
+
+public partial class MainPage
+{
+    enum Columns { Side, Main }
+    enum Rows { Main, Ad }
+    public override void Build() => Content =
+        Grid(
+            Rows(
+                (Rows.Main, 541),
+                (Rows.Ad, 100)
+                ),
+
+            Grid (
+                Columns (
+                    (Columns.Side, 66),
+                    (Columns.Main, 325)
+                    ),
+                Grid ()
+                    .Grid_Column (Columns.Side)
+                    .Background("#ececed"),
+
+                Grid ()
+                    .Grid_Column (Columns.Main)
+            ).Grid_Row(Rows.Main),
+
+            Border(
+                TextBlock("광고문의 010.....")
+                    .FontWeight("Bold")
+                    .VCenter()
+                    .HCenter()
+                    .Background("#f8f8f8")
+
+            )
+            .BorderThicknesses(top:1)
+            .BorderBrush("#dedede")
+            .Grid_Row(Rows.Ad)
+        );
+}
