0a9cba6c7b5fa7ebd3825bb82e9e22a72ffc4a7f
lukewire129 <lukewire129@gmail.com>
2024-05-20 16:55:50 +0900

    KakaoLogin Update .

---
+++ CSharpForMarkupLogin/App.cs
@@ -0,0 +1,34 @@
+﻿using CSharpForMarkupLogin.Views;
+using CSharpMarkup.WPF.Support;
+using System.Windows.Navigation;
+
+namespace CSharpForMarkupLogin;
+
+public class App : MarkupApplication
+{
+    NavigationWindow _navigationWindow;
+    private LoginPage? loginPage;
+    public App()
+    {
+        MainWindow = _navigationWindow = new NavigationWindow ()
+        {
+            Width = 360,
+            Height = 590,
+            ShowsNavigationUI = false,
+            WindowStyle = System.Windows.WindowStyle.None,
+            AllowsTransparency = true,
+        };
+
+        _navigationWindow.PreviewMouseDown += (s, e) =>
+        {
+            _navigationWindow.DragMove ();
+        };
+
+        Go (LoginPage);
+    }
+
+
+    MarkupPage LoginPage => loginPage ??= new LoginPage ();
+    public void Go(MarkupPage page) => _navigationWindow.NavigationService.Navigate (page);
+    public void Back(MarkupPage page) => _navigationWindow.NavigationService.GoBack ();
+}
---
+++ CSharpMarkup.WPF.Support/MarkupPage.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Controls;
+
+namespace CSharpMarkup.WPF.Support;
+
+public abstract class MarkupPage : Page, IBuild
+{
+    public new object Content { get => base.Content; set { base.Content = value is CSharpMarkup.Wpf.DependencyObject d ? d.UI : value; } }
+
+    public abstract void Build();
+}
