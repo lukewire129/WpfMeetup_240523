c0a47c1982c4227a9eb9e393c5074c4cbb87f88d
lukewire129 <lukewire129@gmail.com>
2024-05-21 09:03:28 +0900

    Update .

---
+++ CSharpMarkup.WPF.Support/Luke.Helpers.cs
@@ -0,0 +1,25 @@
+﻿using CSharpMarkup.Wpf;
+using Windows = System.Windows;
+using Controls = System.Windows.Controls;
+namespace CSharpMarkup.WPF.Support;
+
+public static partial class Helpers
+{
+    public static UIElement Row(params Windows.UIElement[] children)
+    {
+        if (children == null)
+            return null;
+
+        var grid = CSharpMarkup.Wpf.Helpers.Grid (children);
+
+        return grid;
+    }
+    public static Grid Grid(UIElement Row)
+    {
+        if (Row == null)
+            return CSharpMarkup.Wpf.Helpers.Grid ();
+
+
+        return Grid (Row);
+    }
+}
