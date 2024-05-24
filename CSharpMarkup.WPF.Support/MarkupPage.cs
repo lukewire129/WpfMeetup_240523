d99fab71eb8e91f2eb4d81283a4686ad4b1506eb
lukewire <lukewire@dmtek.co.kr>
2024-05-24 15:26:52 +0900

    Update .

--- CSharpMarkup.WPF.Support/MarkupPage.cs
+++ CSharpMarkup.WPF.Support/MarkupPage.cs
@@ -6,5 +6,8 @@ public abstract class MarkupPage : Page, IBuild
 {
     public new object Content { get => base.Content; set { base.Content = value is CSharpMarkup.Wpf.DependencyObject d ? d.UI : value; } }
 
-    public abstract void Build();
+    public virtual void Build()
+    {
+
+    }
 }
---
+++ CSharpMarkup.WPF.Support/ViewAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharpMarkup.WPF.Support
+{
+    [AttributeUsage (AttributeTargets.Class, Inherited = false)]
+    public class ViewAttribute : Attribute
+    {
+        public string Description { get; }
+
+        public ViewAttribute()
+        {
+        }
+
+        public ViewAttribute(string description)
+        {
+            Description = description;
+        }
+    }
+}
